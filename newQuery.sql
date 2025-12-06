4460c77eb1c47eff378a32d2c9119ff9d88be88f
Willian <willianmetelien@gmail.com>
2025-12-06 13:25:44 -0400

    extract name and last name of data

--- newQuery.sql
+++ newQuery.sql
@@ -1,19 +1,33 @@
-
 -- INSERT INTO  users(name) VALUES ('Carolin');
  -- update data
 -- UPDATE users SET name = 'Willian' WHERE name = 'Pedro'
-
--- SELECT *
+ -- SELECT *
 -- FROM users
--- LIMIT 2 
+-- LIMIT 2
 -- OFFSET 2;
-
-
-
--- SELECT *
+ -- SELECT *
 -- FROM users WHERE name LIKE 'J%';
-
--- //Eliminar registro
+ -- //Eliminar registro
 -- DELETE FROM users WHERE name LIKE 'J%';
+ -- SELECT name FROM users WHERE id < 5;
+ -- SELECT id,
+--        upper(name) as upper_name,
+--        lower(name) as lower_name,
+--        length(name) as LENGTH
+-- FROM users
+ -- select name,
+--        SUBSTRING(name, 0, 5),
+--        POSITION(' ' in name),
+--        SUBSTRING(name, 0, POSITION(' ' in name)) as first_name,
+--        SUBSTRING(name, POSITION(' ' in name) + 1) as last_name,
+--        TRIM(SUBSTRING(name, POSITION(' ' in name))) as trimmed_last_name
+-- from users;
+ -- SELECT *
+-- from users -- delete from users
 
--- SELECT name FROM users WHERE id < 5;
+UPDATE users
+set first_name = SUBSTRING(name, 0, POSITION(' ' in name)),
+    last_name = SUBSTRING(name, POSITION(' ' in name) + 1) -- select all users
+
+SELECT *
+from users
